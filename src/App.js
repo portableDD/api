1ff0f08c0b2fa4b8022c2aeccaad5ab58e26976a
portable <emmanueltemitopedorcas20@gmail.com>
2020-11-23 12:21:45 +0100

    learnt how to do apiu calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+const gitHubUrl = "https://api.github.com/users/deekshasharma";
 
 function App() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getGiHubUserWithAxios()
+    // getGitHubUserWithFetch();
+  }, []);
+
+  const getGiHubUserWithAxios = async () => {
+    const response = await axios.get(gitHubUrl);
+    setUserData(response.data);
+  };
+
+  // const getGitHubUserWithFetch = async () => {
+  //   const response = await fetch(gitHubUrl);
+  //   const jsonData = await response.json();
+  //   setUserData(jsonData);
+  // };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>GitHub User Data</h2>
       </header>
+      <div className="user-container">
+        <h5 className="info-item">{userData.name}</h5>
+        <h5 className="info-item">{userData.location}</h5>
+        <h5 className="info-item">{userData.blog}</h5>
+        <h5 className="info-item">{userData.company}</h5>
+      </div>
     </div>
   );
 }
